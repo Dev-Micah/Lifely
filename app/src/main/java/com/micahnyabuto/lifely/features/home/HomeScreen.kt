7453da7b51161e5fa215ec5be23cf16c7f5dfa53
Micah Nyabuto <atembamicah@gmail.com>
2025-06-24 00:42:04 +0300

    Add Bottom Bar Screens

---
+++ app/src/main/java/com/micahnyabuto/lifely/features/home/HomeScreen.kt
@@ -0,0 +1,20 @@
+package com.micahnyabuto.lifely.features.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.micahnyabuto.lifely.features.home.components.HomeTopBar
+
+@Composable
+fun HomeScreen(){
+    HomeTopBar()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text("HomeScreen")
+    }
+}
---
+++ app/src/main/java/com/micahnyabuto/lifely/features/home/components/HomeTopBar.kt
@@ -0,0 +1,52 @@
+package com.micahnyabuto.lifely.features.home.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar(){
+
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row {
+                        Text("Welcome ,Micah👋",
+                            fontWeight = FontWeight.Bold)
+                    }
+                },
+                actions = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Profile",
+                        modifier = Modifier
+                            .size(35.dp)
+                            .clip(CircleShape)
+                            .background(Color.White)
+                    )
+                }
+            )
+        }
+    ){
+
+    }
+}
