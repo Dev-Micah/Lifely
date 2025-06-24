72f75406afe8c303b15e8625b6f00e7a4fcb414f
Micah Nyabuto <atembamicah@gmail.com>
2025-06-24 22:44:36 +0300

    Set up Koin Dependency Injection

---
+++ app/src/main/java/com/micahnyabuto/lifely/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.micahnyabuto.lifely.di
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.micahnyabuto.lifely.core.data.local.FoodDatabase
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val appModule = module{
+    single{
+        Room.databaseBuilder(
+            androidContext(),
+            FoodDatabase::class.java,
+            "food"
+        ).build()
+    }
+
+    single {
+        get<FoodDatabase>().foodDao()
+    }
+    single {
+
+    }
+
+}
