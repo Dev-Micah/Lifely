21a2911aa2ec92989a0037c980d0a7f7f56f78d5
Micah Nyabuto <atembamicah@gmail.com>
2025-07-04 17:24:27 +0300

    update

--- app/src/main/java/com/micahnyabuto/lifely/FoodApp.kt
+++ app/src/main/java/com/micahnyabuto/lifely/FoodApp.kt
@@ -3,13 +3,16 @@ package com.micahnyabuto.lifely
 import android.app.Application
 import com.micahnyabuto.lifely.di.appModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class FoodApp: Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
+            androidLogger(Level.DEBUG)
             androidContext(this@FoodApp)
             modules(appModule)
         }
