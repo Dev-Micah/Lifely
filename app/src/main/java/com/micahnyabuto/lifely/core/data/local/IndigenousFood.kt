095aa089719f740a3f8f7d0181a4b394537bf8c0
Micah Nyabuto <atembamicah@gmail.com>
2025-07-04 17:27:35 +0300

    Add IndigenousFood DTO

---
+++ app/src/main/java/com/micahnyabuto/lifely/core/data/local/IndigenousFood.kt
@@ -0,0 +1,9 @@
+package com.micahnyabuto.lifely.core.data.local
+
+data class IndigenousFood(
+    val name: String,
+    val category: String,
+    val calories: Int,
+    val healthBenefits: String,
+    val imageName: String
+)
