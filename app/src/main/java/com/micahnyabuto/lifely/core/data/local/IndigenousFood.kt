30fc20f622cb3897ad8f6426b4de44feee4bdd67
Micah Nyabuto <atembamicah@gmail.com>
2025-07-05 14:06:58 +0300

    updated indegenous foods DTO

--- app/src/main/java/com/micahnyabuto/lifely/core/data/local/IndigenousFood.kt
+++ app/src/main/java/com/micahnyabuto/lifely/core/data/local/IndigenousFood.kt
@@ -1,9 +1,14 @@
 package com.micahnyabuto.lifely.core.data.local
 
+//noinspection SuspiciousImport
+import android.R
+
 data class IndigenousFood(
     val name: String,
     val category: String,
+    val description: String,
+    val benefits: String,
     val calories: Int,
-    val healthBenefits: String,
     val imageName: String
 )
+
